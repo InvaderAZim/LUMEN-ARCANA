c934ef849dfd0475a9d3a30b1d813b163f838cc4
InvaderAZim <99921335+InvaderAZim@users.noreply.github.com>
2026-09-19 19:19:16 +0300

    Remove manual cache versioning from splash-sound.js

--- public/splash-sound.js
+++ public/splash-sound.js
@@ -1,6 +1,6 @@
 (()=>{
   const BASE_VOLUME=0.8;
-  const audio=new Audio('/startup-sound.mp3?v=3.0.0-beta.1-a1');
+  const audio=new Audio('/startup-sound.mp3');
   audio.preload='auto';
   audio.playsInline=true;
 
